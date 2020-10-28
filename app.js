9143f1a8b18d7677ea0ef6542b934dd012e9a1de
Harsh Jha <harshjha872@gmail.com>
2020-10-28 11:10:43 +0530

    added animation fadein and fadeout

--- app.js
+++ app.js
@@ -1,46 +1,71 @@
-let i;
-let image = document.getElementById('img');
-let imageleft = {"opacity":"1"};
-let imageright = {"opacity":"0"};
-let AnimationDuration = {"duration":2000}
+const img = document.getElementById('image');
+const btn = document.getElementById('button');
+let moveright = {
+    opacity: "1"
+};
+let moved = {
+    opacity: "0"
+}
+let animationduration = {
+    "duration": 1000
+}
+let fade1 = {
+    opacity: "1"
+};
+let fade2 = {
+    opacity: "0"
+}
+btn.addEventListener('click', () => { 
+    img.style.display = "block";
+    img.animate([moved, moveright], animationduration).onfinish = fadeout;
+});
+// let nextImage = () => {
+//     img.style.left = 0;
+//     // if(img.style.left == 0){
+//     //     img.animate([fade1,fade2],animationduration);
+//     // }
+//     img.style.background = `url('3.jpg') no-repeat center center/cover`
+//     img.animate([fade2,fade1],animationduration);
+//     // if(btn.addEventListener('click')){
+//     //     img.animate([moveright,moved],animationduration);
+//     // }
+// }
+let fadeinNext = () => {
+    let fade3 = {
+        opacity: "0"
+    }
+    let fade4 = {
+        opacity: "1"
+    }
+    let Animeduration = {
+        "duration": 1000
+    }
+    img.style.left = 0;
+    img.style.background = `url('${random()}.jpg') no-repeat center center/cover`;
+    img.animate([fade3, fade4], Animeduration).onfinish = fadeout;
+}
+let fadeout = () => {
+    let fade5 = {
+        opacity: "0"
+    }
+    let fade6 = {
+        opacity: "1"
+    }
+    let Animeduration = {
+        "duration": 1000
+    }
+    img.animate([fade6, fade5], Animeduration).onfinish = fadeinNext;
 
-    // image.addEventListener('click', function fun() {
-
-    //     image.style.background = `url('${random()}.jpg') no-repeat center center/cover`;
-    //     image.style.animationIterationCount = 'infinite';
-    
-    // });
-function random(){
-    return Math.floor(Math.random() * 17) + 1 ; 
+}
+function random() {
+    return Math.floor(Math.random() * 17) + 1;
 
 }
 
-/*  Without click and after every 2 second the image will changes  */
-
-
-setInterval(function(){
-image.style.background = `url('${random()}.jpg') no-repeat center center/cover`;
-image.animate([imageleft,imageright],AnimationDuration);
-
-},2000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function imagechange(name){
-//     image = document.addEventListener('click');
-
-
-// }
+// let MyImages = new Array();
+// for (let i = 0; i < 18; i++) {
+//     MyImages[i] = i + 1;
+//     function NumberReturn() {
+//         return MyImages[i];
+//     }
+// };
